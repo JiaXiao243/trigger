953dea655ffc9a0d265ca30f2d8b56338efccd40
chenjiawen <chenjiawen@baidu.com>
2020-06-10 17:14:56 +0800

    master

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-echo "iommit status publisher Thisii is a test! auto triggered! Yes it is successfully! "
+echo "master ommit status publisher Thisii is a test! auto triggered! Yes it is successfully! "
