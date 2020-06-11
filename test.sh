b8d6ea7a93ef08815668f25cdc713b62c69a03f7
jiaxiao243@126.com <jiaxiao243@126.com>
2020-06-11 15:13:29 +0800

    pr=12

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-echo "pull=11 test=master comment add* request  yesThis is a parakeet repo delete trigger on teamcity!"
+echo "pull=12 test=master comment add* request  yesThis is a parakeet repo delete trigger on teamcity!"
